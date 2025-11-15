071ce62e77e9e0e14941e09b6662579aba3e647b
IanCurufil <165847605+IanCurufil@users.noreply.github.com>
2025-11-15 20:03:57 -0300

    Add main function with menu for passenger management
    ou shit

---
+++ Funciones/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#include "../Cabeceras_h/Pasajeros.h"
+
+
+int main () {
+    int opcion;
+
+    do {
+        
+        printf("\n--- MENU ---\n");
+        printf("1. Destinos\n");
+        printf("2. Registrar Pasajeros\n");
+        printf("3. Reporte Diario\n");
+        printf("4. Listado de pasajeros\n");
+        printf("0. Salir del programa\n");
+        printf("Elija una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion) {
+            case 1:
+                printf("Mostrando Destinos.....");
+                //mostrarDestinos();
+                break;
+            case 2:
+                printf("Registrando pasajero.....");
+                //RegistrarPasajero(Pasajero, &cantidadPasajeros);
+                break;
+            case 3:
+                printf("Reporte diario....");
+                //reporteDiario();
+                break;
+            case 4:
+                printf("Listado de pasajeros....");
+                //listadoDePasajeros();
+                break;
+            case 0:
+                printf("Saliendo del programa.");
+                break;
+            default:
+                printf("Opcion invalida. Intente nuevamente\n");
+                break;
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
