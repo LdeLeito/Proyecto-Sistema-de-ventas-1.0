758ff2dabbc5ed00a5165958487f7971728de21c
Oleos <leonardo.rua@outlook.es>
2025-11-16 23:43:18 -0300

    Cambio de Ian 2 y falta corrección de errores

---
+++ Funciones/ListarPasajeros.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/Pasajeros.h"
+#include "../headers/Fecha.h"
+#include "../headers/pasajes.h"
+
+void listarPasajeros(struct Pasajero *pasajero ,struct Pasaje *pasaje, struct FechaHora *Fecha ,int cantidadPasajeros) 
+{
+    printf("+----+--------+------------+--------+------------------------------+\n");
+    printf("| ID | Butaca |   Fecha    |  Hora  |         Pasajero             |\n");
+    printf("+----+--------+------------+--------+------------------------------+\n");
+
+    for (int i = 0; i < cantidadPasajeros; i++) {
+        printf("| %-2d | %-6d | %-10i | %-6i | %-28s |\n",
+            pasaje[i].id,
+            pasaje[i].butaca,
+            Fecha[i].dia,
+            Fecha[i].hora,
+            pasajero[i].nombreCompleto
+        );
+    }
+
+    printf("+----+--------+------------+--------+------------------------------+\n");
+}
