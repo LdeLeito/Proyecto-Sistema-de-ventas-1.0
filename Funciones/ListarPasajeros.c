ef38409e6912bd8e7ecc0c2f2d454f7ba8da643c
Oleos <leonardo.rua@outlook.es>
2025-11-19 16:12:04 -0300

    Correciones de errores en el listado de pasajeros

--- Funciones/ListarPasajeros.c
+++ Funciones/ListarPasajeros.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include "../headers/Pasajeros.h"
-#include "../headers/Fecha.h"
 #include "../headers/pasajes.h"
 
-void listarPasajeros(struct Pasajero *pasajero ,struct Pasaje *pasaje, struct FechaHora *Fecha ,int cantidadPasajeros) 
+void ListarPasajesConPasajeros(struct Pasaje *pasajes, int cantidadPasajes, struct Pasajero *pasajeros, int cantidadPasajeros)
 {
     printf("+----+--------+------------+--------+------------------------------+\n");
-    printf("| ID | Butaca |   Fecha    |  Hora  |         Pasajero             |\n");
+    printf("| ID | Butaca |   Fecha    |  Hora  |        Pasajero             |\n");
     printf("+----+--------+------------+--------+------------------------------+\n");
 
-    for (int i = 0; i < cantidadPasajeros; i++) {
-        printf("| %-2d | %-6d | %-10i | %-6i | %-28s |\n",
-            pasaje[i].id,
-            pasaje[i].butaca,
-            Fecha[i].dia,
-            Fecha[i].hora,
-            pasajero[i].nombre, pasajero[i].apellido
-        );
+    for (int i = 0; i < cantidadPasajes; i++)
+    {
+        // Buscar el pasajero asociado al pasaje
+        char nombreCompleto[61] = "Desconocido";
+        for (int j = 0; j < cantidadPasajeros; j++)
+        {
+            if (strcmp(pasajes[i].id_pasajero, pasajeros[j].idpasajero) == 0)
+            {
+                snprintf(nombreCompleto, sizeof(nombreCompleto), "%s %s", pasajeros[j].nombre, pasajeros[j].apellido);
+                break;
+            }
+        }
+
+        printf("| %-2d | %-6d | %-10s | %-6s | %-28s |\n",
+               pasajes[i].id,
+               pasajes[i].butaca,
+               pasajes[i].fecha,
+               pasajes[i].horario,
+               nombreCompleto);
     }
 
     printf("+----+--------+------------+--------+------------------------------+\n");
-}
